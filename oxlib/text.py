16a0511ea6140ed82bb4f7ee75430ed296d03a68
Rolux <rolux@Rolux.local>
2008-07-06 11:43:06 +0200

    wrapString

--- oxlib/text.py
+++ oxlib/text.py
@@ -65,6 +65,37 @@ def wrap(text, width):
                   text.split(' ')
                   )
 
+def wrapString(string, length, separator, balance):
+  if balance:
+      # balance lines: test if same number of lines
+      # can be achieved with a shorter line length
+      lines = wrapString(string, length, separator, False).split(separator)
+      if len(lines) > 1:
+          while length > max(map(lambda x : len(x), string.split(' '))):
+              length -= 1
+              if len(wrapString(string, length, separator, False).split(separator)) > len(lines):
+                  length += 1
+                  break
+  words = string.split(' ')
+  lines = ['']
+  for word in words:
+      if len(lines[len(lines) - 1] + word + ' ') <= length + 1:
+          # word fits in current line
+          lines[len(lines) - 1] += word + ' ';
+      else:
+          if len(word) <= length:
+              # word fits in next line
+              lines.append(word + ' ')
+          else:
+              # word is longer than line
+              position = length - len(lines[len(lines) - 1]) + 1
+              lines[len(lines) - 1] += word[0:position]
+              for i in range(position, len(word), length):
+                  lines.append(word[i:i+length]);
+              lines[len(lines) - 1] += ' '
+  return separator.join(lines).strip()
+
+
 def truncateString(s, num):
     """Truncates a string after a certain number of chacters, but ends with a word
 
