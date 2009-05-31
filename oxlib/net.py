7a4bc7d1f4071d9176acb511adf266a111ef3b89
j <j>
2009-05-31 20:33:51 +0200

    update browser string

--- oxlib/net.py
+++ oxlib/net.py
@@ -11,7 +11,7 @@ from chardet.universaldetector import UniversalDetector
 
 # Default headers for HTTP requests.
 DEFAULT_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9) Gecko/2008061015 Firefox/3.0',
+    'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2a1pre) Gecko/20090529 Minefield/3.6a1pre',
     'Accept-Encoding': 'gzip'
 }
 
