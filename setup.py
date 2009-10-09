3cf8943f083cbc79ee640fbf8f3b3d9fa2d11335
j <j>
2009-10-09 16:53:52 +0200

    only use distutils, add requirements.txt

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 # vi:si:et:sw=4:sts=4:ts=4
 # encoding: utf-8
-from setuptools import setup, find_packages
+from distutils.core import setup
+
+def get_bzr_version():
+    import os
+    rev = int(os.popen('bzr revno').read())
+    if rev:
+        return u'%s' % rev
+    return u'unknown'
 
 setup(
     name="oxlib",
-    version="0.1",
+    version="1.0.%s" % get_bzr_version() ,
     description="python-oxlib some tools to build tools",
     author="0x",
     author_email="code@0xdb.org",
     url="http://code.0xdb.org/oxlib",
     download_url="http://code.0xdb.org/oxlib/download",
     license="GPLv3",
-    packages=find_packages(),
+    packages=['oxlib'],
     zip_safe=False,
-    install_requires=[
-          'simplejson',
-          'chardet',
-    ],
     keywords = [
     ],
     classifiers = [
-        'Development Status :: 3 - Alpha',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
