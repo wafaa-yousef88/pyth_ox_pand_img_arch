9e64aad9c695458571bf2f5848788e6fb7c44756
j <j>
2010-08-12 23:35:31 +0200

    update useragent

--- ox/net.py
+++ ox/net.py
@@ -12,7 +12,10 @@ from chardet.universaldetector import UniversalDetector
 
 # Default headers for HTTP requests.
 DEFAULT_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; U; Linux i386; en-US; rv:1.9.1.1) Gecko/20090716 Firefox/3.5',
+    'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100723 Firefox/3.6.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-us,en;q=0.5',
     'Accept-Encoding': 'gzip'
 }
 
