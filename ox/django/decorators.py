f876221e373df062a02088599434fdde9ef9eb77
j <j>
2011-10-03 18:05:27 +0200

    add admin_required decorator

--- ox/django/decorators.py
+++ ox/django/decorators.py
@@ -19,3 +19,14 @@ def login_required_json(function=None):
 		return render_to_json_response({'status': {'code': 401, 'text': 'login required'}})
 	return wraps(function)(_wrapped_view)
 
+def admin_required_json(function=None):
+    """
+    Decorator for views that checks that the user is logged in
+    return json error if not logged in.
+    """
+
+	def _wrapped_view(request, *args, **kwargs):
+		if request.user.is_authenticated() and request.user.get_profile().get_level() == 'admin':
+			return function(request, *args, **kwargs)
+		return render_to_json_response({'status': {'code': 403, 'text': 'permission denied'}})
+	return wraps(function)(_wrapped_view)
